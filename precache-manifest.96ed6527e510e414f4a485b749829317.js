2d5f9a01768f56f53a1de3d0800e353f9373851f
petermail <petermail@seznam.cz>
2021-01-06 19:32:10 +0100

    Init commit

---
+++ precache-manifest.96ed6527e510e414f4a485b749829317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe27b08462bd73d07240496fc8e11f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ec389238263ae52d565",
+    "url": "/static/css/main.b007f1e3.chunk.css"
+  },
+  {
+    "revision": "eb82e5b7c5028d60747b",
+    "url": "/static/js/2.47585623.chunk.js"
+  },
+  {
+    "revision": "42a31b586fadc7e83342bae79ad2096b",
+    "url": "/static/js/2.47585623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec389238263ae52d565",
+    "url": "/static/js/main.9deaadcc.chunk.js"
+  },
+  {
+    "revision": "8d5a449454e3a0f9cef5",
+    "url": "/static/js/runtime-main.c75e2d5c.js"
+  }
+]);
